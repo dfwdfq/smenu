3fe080b407bd76fb3c4d9b6574195d0710967267
Ian <haskel.list@yandex.ru>
2025-10-17 19:30:17 +0300

    added procedures to read PATH variable and to read dir

--- config.h
+++ config.h
@@ -10,17 +10,29 @@
 */
 #ifndef CONFIG_H
 #define CONFIG_H
-#include<raylib.h>
-#include<getopt.h>
+#include<raylib.h>  
+#include<getopt.h>  
+#include<dirent.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<limits.h>
 
+extern char* PATH;
+extern char** bins; //contains all executable files from $PATH
+extern int bins_amount;
+extern int current_dir_entry;
 
+extern void free_bins(void);
+
+extern void read_path(void);
+extern int read_dir(char* dir);
 
 extern unsigned int top,left, width, height;
 extern unsigned int font_size;
 
 extern Color background_color, selection_color, text_color, selected_text_color;
+
 
 extern void parse_argv(int argc, char** argv);
 
