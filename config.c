f55e6bec762b07c8b715efb4ef973053b68caf5d
Ian <haskel.list@yandex.ru>
2025-10-17 16:54:37 +0300

    config defined via cli

---
+++ config.c
@@ -0,0 +1,44 @@
+#include"config.h"
+
+unsigned int top = 0,
+             width = 200,
+             height = 300;
+
+unsigned int font_size = 14;
+
+Color background_color = BLACK;
+Color selection_color = BLUE;
+Color text_color = WHITE;
+Color selected_text_color = YELLOW;
+
+void parse_argv(int argc, char **argv)
+{
+  opterr = 0;
+  
+  int c;
+  while((c = getopt(argc,argv,"t:w:h:")) != -1)
+    {
+      switch (c)
+	{
+	case 't':
+	  top = atoi(optarg);
+	  break;
+	case 'w':
+	  width = atoi(optarg);
+	  break;
+	case 'h':
+	  height = atoi(optarg);
+	  break;
+	case '?':
+	  {
+	    if(optopt)
+	      fprintf(stderr,"Error: Option -%c requires an argument.\n",optopt);
+	    else
+	      fprintf(stderr,"Error: Unknown option: %s\n",argv[optind-1]);
+
+	    exit(-1);
+	  }
+	  break;
+	}
+    }
+}
---
+++ config.h
@@ -0,0 +1,27 @@
+/*
+  the application follows suckless approach,
+  so nothing external is required by application
+  to run and it's configuration is stored in-memory.
+  To configure the app you can change values in config.c
+  or use CLI arguments.
+
+  This header contains config definitions and CLI arguments
+  processing procedures.
+*/
+#ifndef CONFIG_H
+#define CONFIG_H
+#include<raylib.h>
+#include<getopt.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define STR_BUFFER_LEN 1024
+
+extern unsigned int top, width, height;
+extern unsigned int font_size;
+
+extern Color background_color, selection_color, text_color, selected_text_color;
+
+extern void parse_argv(int argc, char** argv);
+
+#endif
